a50283f445fe0c40d1f5d102b0500c091ab5f01f
mark sevelj <31756570+imAsparky@users.noreply.github.com>
2021-09-24 18:24:18 +0800

    chore(sphinx): Update package docs structure #128 (#130)
    * chore(sem_ver): Add settings to setup.cfg #127
    
    These settings did not exist for the package.
    
    closes #127
    
    * chore(sphinx): Update package docs structure #128
    
    Sphinx docs structure was not separated.
    
    closes #128

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -193,11 +193,11 @@ def test_bake_without_author_file(cookies):
     ) as result:
         found_toplevel_files = [f.basename for f in result.project.listdir()]
         assert "AUTHORS.rst" not in found_toplevel_files
-        doc_files = [f.basename for f in result.project.join("docs").listdir()]
+        doc_files = [f.basename for f in result.project.join("docs/source").listdir()]
         assert "authors.rst" not in doc_files
 
         # Assert there are no spaces in the toc tree
-        docs_index_path = result.project.join("docs/index.rst")
+        docs_index_path = result.project.join("docs/source/index.rst")
         with open(str(docs_index_path)) as index_file:
             assert "contributing\n   history" in index_file.read()
 
